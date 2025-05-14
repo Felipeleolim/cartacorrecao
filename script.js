c250a5c2b04bc714694d3816db4bf5b29bf699e0
Felipeleolim <161047774+Felipeleolim@users.noreply.github.com>
2025-05-14 16:50:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -575,10 +575,8 @@ function formatDate(dateString) {
     return date.toLocaleDateString('pt-BR', {
       day: '2-digit',
       month: '2-digit',
-      year: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit'
-    });
+      year: 'numeric'
+          });
   } catch (e) {
     return dateString; // Retorna o valor original se não puder formatar
   }
